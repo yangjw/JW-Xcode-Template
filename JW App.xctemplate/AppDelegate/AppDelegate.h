413a0843729412faef8c003714d2cdff0900b14d
yangjw <yangjw.kai7@gmail.com>
2016-07-14 14:32:40 +0800

    添加

---
+++ JW App.xctemplate/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JWKitDemo
+//
+//  Created by njdby on 16/7/5.
+//  Copyright © 2016年 njdby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JW Application.xctemplate/General/Macro/APINet.h
@@ -0,0 +1,14 @@
+//
+//  APINet.h
+//
+//  Created by njdby on 16/2/29.
+//  Copyright © 2016年 njdby. All rights reserved.
+//
+
+#ifndef APINet_h
+#define APINet_h
+
+//#define kNetAPIBase      @""
+//#define KNetApiHtml      @""
+
+#endif /* APINet_h */
